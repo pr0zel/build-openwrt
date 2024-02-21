396f927857848d2ff05f0ac0b60cf5d307be9611
pr0zel <pr0zel@gmail.com>
2024-02-21 22:35:27 +0800

    Scaling governor

--- config/immortalwrt-master/diy-part2.sh
+++ config/immortalwrt-master/diy-part2.sh
@@ -31,5 +31,8 @@
 # ------------------------------- Other ends -------------------------------
 
 sed -i 's/CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND=y/CONFIG_CPU_FREQ_DEFAULT_GOV_SCHEDUTIL=y/g' target/linux/x86/64/config-5.15
-sed -i 's/# CONFIG_CPU_FREQ_GOV_CONSERVATIVE is not set/CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y/g' target/linux/x86/64/config-5.15
+#sed -i 's/# CONFIG_CPU_FREQ_GOV_CONSERVATIVE is not set/CONFIG_CPU_FREQ_GOV_CONSERVATIVE=y/g' target/linux/x86/64/config-5.15
+echo "CONFIG_CPU_FREQ_GOV_CONSERVATIVE=m\n" >> target/linux/x86/64/config-5.15
+
+
 
